db4a65ac6b478f94ba80f809534cec0df3c9645d
Bins19 <badianebina@gmail.com>
2023-02-12 13:55:03 +0100

    JPA Enumerations

---
+++ src/main/java/guru/springframework/springrecipeapp/models/Difficulty.java
@@ -0,0 +1,10 @@
+package guru.springframework.springrecipeapp.models;
+
+public enum Difficulty {
+
+    EASY,
+    MODERATE,
+    KIND_OF_HARD,
+    HARD
+
+}
--- src/main/java/guru/springframework/springrecipeapp/models/Recipe.java
+++ src/main/java/guru/springframework/springrecipeapp/models/Recipe.java
@@ -17,8 +17,8 @@ public class Recipe {
     private String source;
     private String url;
     private String directions;
-    //TODO
-    //private Difficulty difficulty;
+    @Enumerated(value = EnumType.STRING)
+    private Difficulty difficulty;
     @Lob
     private Byte[] image;
     @OneToOne(cascade = CascadeType.ALL)
@@ -88,6 +88,14 @@ public class Recipe {
 
     public void setDirections(String directions) {
         this.directions = directions;
+    }
+
+    public Difficulty getDifficulty() {
+        return difficulty;
+    }
+
+    public void setDifficulty(Difficulty difficulty) {
+        this.difficulty = difficulty;
     }
 
     public Byte[] getImage() {
