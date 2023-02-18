43df198b54387023bcb62c17420493b23da3a41c
Bins19 <badianebina@gmail.com>
2023-02-19 00:10:43 +0100

    Using Project Lombook

--- src/main/java/guru/springframework/springrecipeapp/models/Category.java
+++ src/main/java/guru/springframework/springrecipeapp/models/Category.java
@@ -1,9 +1,16 @@
 package guru.springframework.springrecipeapp.models;
 
 import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.Hibernate;
 
+import java.util.Objects;
 import java.util.Set;
 
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
 @Entity
 public class Category {
 
@@ -12,30 +19,19 @@ public class Category {
     private Long id;
     private String description;
     @ManyToMany(mappedBy = "categories")
+    @ToString.Exclude
     private Set<Recipe> recipes;
 
-    public Long getId() {
-        return id;
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+        Category category = (Category) o;
+        return id != null && Objects.equals(id, category.id);
     }
 
-    public void setId(Long id) {
-        this.id = id;
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
     }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public Set<Recipe> getRecipes() {
-        return recipes;
-    }
-
-    public void setRecipes(Set<Recipe> recipes) {
-        this.recipes = recipes;
-    }
-
 }
--- src/main/java/guru/springframework/springrecipeapp/services/RecipeService.java
+++ src/main/java/guru/springframework/springrecipeapp/services/RecipeService.java
@@ -2,11 +2,13 @@ package guru.springframework.springrecipeapp.services;
 
 import guru.springframework.springrecipeapp.models.Recipe;
 import guru.springframework.springrecipeapp.repositories.RecipeRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
+@Slf4j
 public class RecipeService {
 
     private final RecipeRepository recipeRepository;
@@ -16,6 +18,7 @@ public class RecipeService {
     }
 
     public List<Recipe> getAll() {
+        log.debug("I'm in the service");
         return (List<Recipe>) recipeRepository.findAll();
     }
 
