f60f246c373a2797976e8a23178765ce2612c20b
Bins19 <badianebina@gmail.com>
2023-02-19 17:55:22 +0100

    Mockito Argument Capture

--- src/test/java/guru/springframework/springrecipeapp/controllers/IndexControllerTest.java
+++ src/test/java/guru/springframework/springrecipeapp/controllers/IndexControllerTest.java
@@ -1,16 +1,20 @@
 package guru.springframework.springrecipeapp.controllers;
 
+import guru.springframework.springrecipeapp.models.Recipe;
 import guru.springframework.springrecipeapp.services.RecipeService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.ui.Model;
 
+import java.util.Arrays;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.*;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.*;
 
 class IndexControllerTest {
 
@@ -28,11 +32,15 @@ class IndexControllerTest {
 
     @Test
     void getIndexPage() {
+        when(recipeService.getAll()).thenReturn(Arrays.asList(new Recipe(), new Recipe()));
+        ArgumentCaptor<List<Recipe>> argumentCaptor = ArgumentCaptor.forClass(List.class);
+
         String index = controller.getIndexPage(model);
 
         assertEquals(index, "index");
-        verify(model, times(1)).addAttribute(eq("recipes"), anyList());
+        verify(model, times(1)).addAttribute(eq("recipes"), argumentCaptor.capture());
         verify(recipeService, times(1)).getAll();
+        assertEquals(argumentCaptor.getValue().size(), 2);
     }
 
 }
