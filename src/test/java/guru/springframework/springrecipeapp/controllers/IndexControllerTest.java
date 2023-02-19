f0068ec40ea64594efe96ea5e80fa0f1c8d2d3f0
Bins19 <badianebina@gmail.com>
2023-02-19 14:24:40 +0100

    Assignment - Write Test for IndexController

---
+++ src/test/java/guru/springframework/springrecipeapp/controllers/IndexControllerTest.java
@@ -0,0 +1,38 @@
+package guru.springframework.springrecipeapp.controllers;
+
+import guru.springframework.springrecipeapp.services.RecipeService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.ui.Model;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+class IndexControllerTest {
+
+    IndexController controller;
+    @Mock
+    RecipeService recipeService;
+    @Mock
+    Model model;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.initMocks(this);
+        controller = new IndexController(recipeService);
+    }
+
+    @Test
+    void getIndexPage() {
+        String index = controller.getIndexPage(model);
+
+        assertEquals(index, "index");
+        verify(model, times(1)).addAttribute(eq("recipes"), anyList());
+        verify(recipeService, times(1)).getAll();
+    }
+
+}
