f96ac8993fe11b4112b9bf3591a553b2524ebfaf
Bins19 <badianebina@gmail.com>
2023-03-09 22:35:58 +0100

    Create a Recipe

--- src/main/java/guru/springframework/springrecipeapp/controllers/RecipeController.java
+++ src/main/java/guru/springframework/springrecipeapp/controllers/RecipeController.java
@@ -1,9 +1,12 @@
 package guru.springframework.springrecipeapp.controllers;
 
+import guru.springframework.springrecipeapp.commands.RecipeCommand;
 import guru.springframework.springrecipeapp.services.RecipeService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -20,5 +23,19 @@ public class RecipeController {
         model.addAttribute("recipe", service.getById(Long.parseLong(id)).get());
 
         return "recipe/show";
+    }
+
+    @RequestMapping("/recipe/new")
+    public String newRecipe(Model model) {
+        model.addAttribute("recipe", new RecipeCommand());
+
+        return "recipe/form";
+    }
+
+    @PostMapping("/recipe")
+    public String saveOrUpdate(@ModelAttribute RecipeCommand recipeCommand) {
+        RecipeCommand savedRecipeCommand = service.saveRecipeCommand(recipeCommand);
+
+        return "redirect:/recipe/show/" + savedRecipeCommand.getId();
     }
 }
